ca207d4bccb501bd592ce4be74ab8b82365ea8dd
Miguel Domingo <midobal9@gmail.com>
2021-01-20 13:22:33 +0100

    Updated conda installation.

--- full_installation.sh
+++ full_installation.sh
@@ -29,13 +29,14 @@ git clone https://github.com/PRHLT/nmt-keras_practicas-TA.git "$PTH"/nmt-keras
 ##########################
 wget https://repo.continuum.io/miniconda/Miniconda3-py37_4.8.3-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p "$PTH"/miniconda
-export PATH="$PTH/miniconda/bin:$PATH"
+export conda=${PTH}/miniconda/bin/conda
 hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-conda install --file "$PTH"/nmt-keras/req-travis-conda.txt
-conda install mkl mkl-service
-pip install -r "$PTH"/nmt-keras/req-travis-pip.txt
-pip install tensorflow==1.14.0
+${conda} config --set always_yes yes --set changeps1 no
+${conda} update -q conda
+${conda} info -a
+${conda} install --file "$PTH"/nmt-keras/req-travis-conda.txt
+${conda} install mkl mkl-service pip
+export pip=${PTH}/miniconda/bin/pip
+${pip} install -r "$PTH"/nmt-keras/req-travis-pip.txt
+${pip} install tensorflow==1.14.0
 rm miniconda.sh
